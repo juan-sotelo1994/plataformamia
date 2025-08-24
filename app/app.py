3cb64bc33b2dc9cc2e8cb5a02ded0f79ba0589a7
Juan David Sotelo <131626247+juan-sotelo1994@users.noreply.github.com>
2025-08-24 11:31:03 -0500

    version1

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+#registro de las importaciones y configuraciones
+
+
+# registro de los blueprints
+
+
+# ejecuición de la aplicación
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5711)
+    
